3535075c0e22d217cfdc559fb5b5c6a0c27c1272
alexestebanupc <alex.esteban.rodriguez@estudiantat.upc.edu>
2023-05-07 23:48:23 +0200

    a

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import express from "express";
 import db from "./config/mongo";
 import { router } from "./routes";
-// hooks!
+// hook!
 
 const PORT = process.env.PORT || 3001;  
 const app = express();
