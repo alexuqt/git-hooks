e3e37c678c1d6fb6c32356bfe4719a5b9588b58f
alexestebanupc <alex.esteban.rodriguez@estudiantat.upc.edu>
2023-05-10 19:53:36 +0200

    feat: use symbolic link

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import express from "express";
 import db from "./config/mongo";
 import { router } from "./routes";
-// hook!sisassaas
+// hook!sisassaass
 
 const PORT = process.env.PORT || 3001;  
 const app = express();
